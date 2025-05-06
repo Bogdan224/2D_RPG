848ad8c8f29a43a1132e6272459f41713187c5e5
Богдан Колганов <kolganov_25@mail.ru>
2025-05-06 17:25:42 +0700

    add new animated sprites

--- Assets/Scripts/Elevation_Exit.cs
+++ Assets/Scripts/Elevation_Exit.cs
@@ -2,26 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Elevation_Entry : MonoBehaviour
+public class Elevation_Exit : MonoBehaviour
 {
     public Collider2D[] mountainColliders;
     public Collider2D[] boundaryColliders;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.gameObject.tag == "Player")
+        if (collision.gameObject.tag == "Player")
         {
             foreach (var item in mountainColliders)
-            {
-                item.enabled = false;
-            }
-
-            foreach (var item in boundaryColliders)
             {
                 item.enabled = true;
             }
 
-            collision.gameObject.GetComponent<SpriteRenderer>().sortingOrder = 15;
+            foreach (var item in boundaryColliders)
+            {
+                item.enabled = false;
+            }
+
+            collision.gameObject.GetComponent<SpriteRenderer>().sortingOrder = 5;
         }
     }
 }
